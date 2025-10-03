3f1ac58b74379df335235e81b5d6e525c58b9161
Rafael Ferreira Pimentel <r.ferreira.pimentel@avanade.com>
2025-10-03 15:45:54 -0300

    Remove `Get` method from ProductController

--- src/Products.API/Controllers/ProductController.cs
+++ src/Products.API/Controllers/ProductController.cs
@@ -76,19 +76,5 @@ namespace Products.API.Controllers
             _products.Remove(product);
             return NoContent();
         }
-
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
-        {
-            // Consulta “fake” + regra de negócio + formatação tudo junto
-            var product = _products.FirstOrDefault(x => x.Id == id);
-            if (product == null)
-            {
-                // Mensagem vaga
-                return StatusCode(500, "Internal Server Error");
-            }
-            // Retorna entidade crua sem DTO
-            return Ok(product);
-        }
     }
 }
